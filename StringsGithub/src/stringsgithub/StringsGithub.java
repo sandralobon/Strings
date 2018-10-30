87e0fc836dc348670898f65c4d9796fbc817ad47
admin <admin@Win7_1TB>
2018-10-30 09:46:33 +0100

    Inicial

--- StringsGithub/src/stringsgithub/StringsGithub.java
+++ StringsGithub/src/stringsgithub/StringsGithub.java
@@ -1,21 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Programa que lee una cadena y un carácter y cuenta el número de 
+veces que aparece dicho carácter en la cadena.
  */
 package stringsgithub;
-
-/**
- *
- * @author admin
- */
+import java.util.Scanner;
 public class StringsGithub {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("'Amo a probá");
+        String cadena; // Las cadenas empiezan en 0
+        int cont=0;
+        Scanner sc=new Scanner (System.in);
+        System.out.println("¿Por qué letra empieza?");
+        char caracter=sc.nextLine().charAt(0);
+        caracter=Character.toUpperCase(caracter);
+        System.out.println("Anota cadena");
+        cadena=sc.nextLine();
+        cadena=cadena.toUpperCase(); // Hace mayúsculas las cadenas
+        for(int i=0; i<cadena.length(); i++){
+            char letra=cadena.charAt(i);
+            if (letra==caracter)
+                cont++;
+        }
+        System.out.println("La letra "+ caracter +" se repite "+cont+ " veces");
     }
     
 }
